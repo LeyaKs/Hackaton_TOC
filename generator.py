d5adb848a286cd3350b20ae8ffc5fcce17961e67
grechishnikov.ma <grechishnikov.ma@dozen.mephi.ru>
2024-10-01 09:29:40 +0300

    Added TOC generator from dict

---
+++ generator.py
@@ -0,0 +1,53 @@
+import pymupdf
+import math
+
+g_font = "times-roman"
+g_fontsize = 11
+
+g_line_spacing = 10
+g_indent = 30
+
+g_page_width = 595
+g_page_height = 842
+
+g_drawer_y : float = 30
+
+def __gen_rect(rank: int, header: str) -> pymupdf.Rect:
+    global g_drawer_y
+    drawer_x = __indent(rank)
+    textbox_width = g_page_width - drawer_x - g_indent
+    text_len = pymupdf.get_text_length(header, g_font, g_fontsize)
+    rows_number = math.ceil(text_len / textbox_width)
+    textbox = pymupdf.Rect(drawer_x, g_drawer_y, g_page_width, g_drawer_y + g_fontsize * rows_number * 2)
+    g_drawer_y += g_fontsize * rows_number * 2
+    return textbox
+
+def __indent(rank: int) -> float:
+    atan_res = math.atan((rank - 1) / 5) / math.pi
+    return g_indent + atan_res * g_page_width
+
+def generate(dictionary: dict, input_filename: str, output_filename: str):
+    global g_drawer_y
+    doc = pymupdf.open(input_filename)
+    toc_page_number : int = 0
+    toc_page : pymupdf.Page = doc.new_page(toc_page_number, width = g_page_width, height = g_page_height)
+    toc_page.insert_font(g_font)
+    for header in dictionary:
+        if(g_drawer_y >= g_page_height):
+            toc_page_number += 1
+            toc_page : pymupdf.Page = doc.new_page(toc_page_number, width = g_page_width, height = g_page_height)
+            g_drawer_y = 30
+        textbox = __gen_rect(dictionary[header][0], header)
+        toc_page.insert_textbox(textbox, header, fontsize=g_fontsize, fontname=g_font)
+        link = toc_page.insert_link({"kind": pymupdf.LINK_GOTO, "page": dictionary[header][1] + toc_page_number, "from": textbox, "to": pymupdf.Point(1, 1)})
+    doc.save(output_filename)
+
+
+def main():
+    dictionary = {}
+    for i in range(1, 100):
+        dictionary["Chapter" * i + str(i)] = (i, 1)
+    generate(dictionary, "test.pdf", "res.pdf")
+
+if __name__ == "__main__":
+    main()
