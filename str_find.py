9b77c4aabc5f6815a55f14974ae2a54325847588
simbon4ik <karpov.ivan123@mail.ru>
2024-10-02 23:31:26 +0300

    final changes

--- str_find.py
+++ str_find.py
@@ -17,37 +17,58 @@ def analyze_font_sizes(pdf_path):
                     line_text = spans["text"]
                     line_text_len = len(line_text)          #Проверка длины (не добавлять слишком длинные и короткие)
                     if(line_text_len < 60 and line_text_len > 3):
-                        page_lines[line_text] = (int(round(spans['size'])), "Bold" in spans['font'])    
-            if (page_lines):
-                sort_fonts = sorted(page_lines.items(), key = lambda item: item[1][0], reverse = True)     #Сортировка по размерам шрифта
-                size_s.append(sort_fonts)
-                fonts_set = set()
+                        page_lines[line_text] = (int(round(spans['size'])), "Bold" in spans['font'])  
+     
+        if (page_lines):
+            sort_fonts = sorted(page_lines.items(), key = lambda item: item[1][0], reverse = True)     #Сортировка по размерам шрифта
+            size_s.append(sort_fonts)
+            fonts_set = set()
+            for i in range(0, len(size_s[counter-1])):
+                fonts_set.add(size_s[counter-1][i][1][0])
+            cnt_size_s = len(fonts_set)
+            if cnt_size_s == 1:
                 for i in range(0, len(size_s[counter-1])):
-                    fonts_set.add(size_s[counter-1][i][1][0])
-                cnt_size_s = len(fonts_set)
-                if cnt_size_s == 1:
-                    for i in range(0, len(size_s[counter-1])):
-                        if size_s[counter-1][i][1][1] == True:
-                            result[size_s[counter-1][i][0]] = (1, counter_pages)
-                    counter += 1
-                    continue
-                elif cnt_size_s == 2:
-                    max_size = sort_fonts[0][1][0]
-                    for i in range(0, len(size_s[counter-1])):
-                        if (size_s[counter-1][i][1][0] == max_size):
-                            result[size_s[counter-1][i][0]] = (1, counter_pages)
-                elif cnt_size_s == 3:
-                    max_size_1 = sort_fonts[0][1][0]
-                    max_size_2 = sort_fonts[1][1][0]
-                    for i in range(0, len(size_s[counter-1])):
-                        if (size_s[counter-1][i][1][0] == max_size_1):
-                            result[size_s[counter-1][i][0]] = (1, counter_pages)
-                        if (size_s[counter-1][i][1][0] == max_size_2):
-                            result[size_s[counter-1][i][0]] = (2, counter_pages)
-                if not result:
-                    for i in range(0, len(size_s[counter-1])):
-                        if size_s[counter-1][i][1][1] == True:
-                            result[size_s[counter-1][i][0]] = (1, counter_pages)
+                    if size_s[counter-1][i][1][1] == True:
+                        result[size_s[counter-1][i][0]] = (1, counter_pages)
                 counter += 1
+                continue
+            elif cnt_size_s == 2:
+                print(size_s[counter-1])
+                max_size = sort_fonts[0][1][0]
+                cnt = 0
+                cnt_all = 0
+                for j in range(0, len(sort_fonts)):
+                    if sort_fonts[j][1][0] == max_size:
+                        cnt += 1 
+                    cnt_all += 1
+                if (cnt/cnt_all > 0,3):
+                    continue
+                for i in range(0, len(size_s[counter-1])):
+                    if (size_s[counter-1][i][1][0] == max_size):
+                        print(size_s[counter-1][i][0])
+                        result[size_s[counter-1][i][0]] = (1, counter_pages)
+            elif cnt_size_s == 3:
+                max_size_1 = sort_fonts[0][1][0]
+                max_size_2 = sort_fonts[1][1][0]
+                cnt = 0
+                cnt_all = 0
+                for j in range(0, len(sort_fonts)):
+                    if sort_fonts[j][1][0] == max_size_2:
+                        cnt += 1 
+                    cnt_all += 1
+                if (cnt/cnt_all > 0,3):
+                    status = 0
+                else:
+                    status = 1
+                for i in range(0, len(size_s[counter-1])):
+                    if (size_s[counter-1][i][1][0] == max_size_1):
+                        result[size_s[counter-1][i][0]] = (1, counter_pages)
+                    if (size_s[counter-1][i][1][0] == max_size_2) and (status == 1):
+                        result[size_s[counter-1][i][0]] = (2, counter_pages)
+            if not result:
+                for i in range(0, len(size_s[counter-1])):
+                    if size_s[counter-1][i][1][1] == True:
+                        result[size_s[counter-1][i][0]] = (1, counter_pages)
+            counter += 1
         counter_pages += 1
     return result
