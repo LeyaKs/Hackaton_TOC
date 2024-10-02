28f336c960843e55ff78384e49425f2966b2b4e7
simbon4ik <karpov.ivan123@mail.ru>
2024-10-02 11:50:06 +0300

    fix func str_find.py

--- str_find.py
+++ str_find.py
@@ -3,24 +3,23 @@ from pymupdf import *
 def analyze_font_sizes(pdf_path):
     size_s = list()          #Множество кеглей
     doc = open("test.pdf")
-    counter = 1
-    result = dict()
+    counter = 1             #Счетчик страниц
+    result = dict()         #Итоговый словарь
 
     for page in doc:
-        text = page.get_text("dict")
-        blocks = text["blocks"]
-        page_lines = dict() 
+        text = page.get_text("dict")        #Получение текста в виде словарей
+        blocks = text["blocks"]             #Разбиение его на блоки
+        page_lines = dict()                 #Словарь для каждой страницы с типом строка, размер шрифта
         for blocks_index in range(0, len(blocks)):
-            #print(blocks[blocks_index])
-            if(blocks[blocks_index]["type"] == 0):
+            if(blocks[blocks_index]["type"] == 0):          #Проверка, что нужный тип страницы
                 for line in blocks[blocks_index]["lines"]:
-                    spans = line["spans"][0]
+                    spans = line["spans"][0]                #Получение нужного значения из line
                     line_text = spans["text"]
-                    line_text_len = len(line_text)
+                    line_text_len = len(line_text)          #Проверка длины (не добавлять слишком длинные и короткие)
                     if(line_text_len < 60 and line_text_len > 3):
                         page_lines[int(round(spans['size']))] = line_text
         if (page_lines):
-            sort_fonts = sorted(page_lines.items(), key = lambda item: item[0], reverse = True)
+            sort_fonts = sorted(page_lines.items(), key = lambda item: item[0], reverse = True)     #Сортировка по размерам шрифта
             size_s.append(sort_fonts)
             cnt_size_s = len(size_s[counter-1])
             if cnt_size_s == 1:
@@ -40,6 +39,3 @@ def analyze_font_sizes(pdf_path):
                         result[size_s[counter-1][i][1]] = (2,counter)
         counter += 1
     return result
-
-pdf_path = "test.pdf"
-print(analyze_font_sizes(pdf_path))
