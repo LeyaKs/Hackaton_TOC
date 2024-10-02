47ce3d1abec345f1d02745303b7bd76ca0af5464
simbon4ik <karpov.ivan123@mail.ru>
2024-10-03 00:34:44 +0300

    secret commit

--- str_find.py
+++ str_find.py
@@ -17,11 +17,11 @@ def analyze_font_sizes(pdf_path):
                     line_text = spans["text"]
                     line_text_len = len(line_text)          #Проверка длины (не добавлять слишком длинные и короткие)
                     if(line_text_len < 60 and line_text_len > 3):
-                        page_lines[line_text] = (int(round(spans['size'])), "Bold" in spans['font'])  
-     
+                         page_lines[line_text] = (int(round(spans['size'])), "Bold" in spans['font'])  
         if (page_lines):
             sort_fonts = sorted(page_lines.items(), key = lambda item: item[1][0], reverse = True)     #Сортировка по размерам шрифта
             size_s.append(sort_fonts)
+            important_status = 0
             fonts_set = set()
             for i in range(0, len(size_s[counter-1])):
                 fonts_set.add(size_s[counter-1][i][1][0])
@@ -31,9 +31,9 @@ def analyze_font_sizes(pdf_path):
                     if size_s[counter-1][i][1][1] == True:
                         result[size_s[counter-1][i][0]] = (1, counter_pages)
                 counter += 1
+                counter_pages += 1
                 continue
             elif cnt_size_s == 2:
-                print(size_s[counter-1])
                 max_size = sort_fonts[0][1][0]
                 cnt = 0
                 cnt_all = 0
@@ -42,10 +42,13 @@ def analyze_font_sizes(pdf_path):
                         cnt += 1 
                     cnt_all += 1
                 if (cnt/cnt_all > 0,3):
+                    counter_pages += 1
+                    counter += 1
                     continue
                 for i in range(0, len(size_s[counter-1])):
                     if (size_s[counter-1][i][1][0] == max_size):
                         result[size_s[counter-1][i][0]] = (1, counter_pages)
+                        important_status = 1
             elif cnt_size_s == 3:
                 max_size_1 = sort_fonts[0][1][0]
                 max_size_2 = sort_fonts[1][1][0]
@@ -62,9 +65,11 @@ def analyze_font_sizes(pdf_path):
                 for i in range(0, len(size_s[counter-1])):
                     if (size_s[counter-1][i][1][0] == max_size_1):
                         result[size_s[counter-1][i][0]] = (1, counter_pages)
+                        important_status = 1
                     if (size_s[counter-1][i][1][0] == max_size_2) and (status == 1):
                         result[size_s[counter-1][i][0]] = (2, counter_pages)
-            if not result:
+                        important_status = 1
+            if (not result) or (important_status == 0):
                 for i in range(0, len(size_s[counter-1])):
                     if size_s[counter-1][i][1][1] == True:
                         result[size_s[counter-1][i][0]] = (1, counter_pages)
