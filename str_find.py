b72ceb7869f43996db082bc25706f5dd51ae1665
simbon4ik <karpov.ivan123@mail.ru>
2024-10-02 21:29:52 +0300

    update str_find

--- str_find.py
+++ str_find.py
@@ -2,9 +2,12 @@ from pymupdf import *
 
 def analyze_font_sizes(pdf_path):
     size_s = list()          #Множество кеглей
-    doc = open("test.pdf")
-    counter = 1             #Счетчик страниц
+    doc = open(pdf_path)
+    counter_pages = 1             #Счетчик страниц
+    counter = 1
     result = dict()         #Итоговый словарь
+# [{'size': 9.0, 'flags': 16, 'font': 'Arial,Bold', 'color': 0, 'ascender': 0.9052734375, 'descender': -0.2119140625, 'text': '1.ОБЩИЕ ПОЛОЖЕНИЯ. ', 'origin': (70.94400024414062, 76.79998779296875), 'bbox': (70.94400024414062, 67.16999053955078, 181.08200073242188, 79.56298828125)}], 'wmode': 0, 'dir':
+
 
     for page in doc:
         text = page.get_text("dict")        #Получение текста в виде словарей
@@ -17,25 +20,40 @@ def analyze_font_sizes(pdf_path):
                     line_text = spans["text"]
                     line_text_len = len(line_text)          #Проверка длины (не добавлять слишком длинные и короткие)
                     if(line_text_len < 60 and line_text_len > 3):
-                        page_lines[int(round(spans['size']))] = line_text
-        if (page_lines):
-            sort_fonts = sorted(page_lines.items(), key = lambda item: item[0], reverse = True)     #Сортировка по размерам шрифта
-            size_s.append(sort_fonts)
-            cnt_size_s = len(size_s[counter-1])
-            if cnt_size_s == 1:
-                continue
-            if cnt_size_s == 2:
-                max_size = sort_fonts[0][0]
+                        page_lines[line_text] = (int(round(spans['size'])), "Bold" in spans['font'])    
+            if (page_lines):
+                sort_fonts = sorted(page_lines.items(), key = lambda item: item[1][0], reverse = True)     #Сортировка по размерам шрифта
+                size_s.append(sort_fonts)
+                fonts_set = set()
                 for i in range(0, len(size_s[counter-1])):
-                    if (size_s[counter-1][i][0] == max_size):
-                        result[size_s[counter-1][i][1]] = (1,counter)
-            if cnt_size_s == 3:
-                max_size_1 = sort_fonts[0][0]
-                max_size_2 = sort_fonts[1][0]
-                for i in range(0, len(size_s[counter-1])):
-                    if (size_s[counter-1][i][0] == max_size_1):
-                        result[size_s[counter-1][i][1]] = (1,counter)
-                    if (size_s[counter-1][i][0] == max_size_2):
-                        result[size_s[counter-1][i][1]] = (2,counter)
-        counter += 1
+                    fonts_set.add(size_s[counter-1][i][1][0])
+                cnt_size_s = len(fonts_set)
+                if cnt_size_s == 1:
+                    for i in range(0, len(size_s[counter-1])):
+                        if size_s[counter-1][i][1][1] == True:
+                            result[size_s[counter-1][i][0]] = (1, counter_pages)
+                    counter += 1
+                    continue
+                elif cnt_size_s == 2:
+                    max_size = sort_fonts[0][1][0]
+                    for i in range(0, len(size_s[counter-1])):
+                        if (size_s[counter-1][i][1][0] == max_size):
+                            result[size_s[counter-1][i][0][0]] = (1,counter_pages)
+                elif cnt_size_s == 3:
+                    max_size_1 = sort_fonts[0][1][0]
+                    max_size_2 = sort_fonts[1][1][0]
+                    for i in range(0, len(size_s[counter-1])):
+                        if (size_s[counter-1][i][1][0] == max_size_1):
+                            result[size_s[counter-1][i][0]] = (1,counter_pages)
+                        if (size_s[counter-1][i][1][0] == max_size_2):
+                            result[size_s[counter-1][i][0]] = (2,counter_pages)
+                if not result:
+                    for i in range(0, len(size_s[counter-1])):
+                        if size_s[counter-1][i][1][1] == True:
+                            result[size_s[counter-1][i][0]] = (1, counter_pages)
+                counter += 1
+        counter_pages += 1
     return result
+
+pdf_path = "test2.pdf"
+print(analyze_font_sizes(pdf_path))
